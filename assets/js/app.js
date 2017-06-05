525aef33c115bdde9a03d341be7355d59b3c0804
Wendy Reyes <wendy.reyessan@gmail.com>
2017-06-05 16:34:26 -0500

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const render = (root) => {
+	root.empty();
+	const wrapper = $("<div class="wrapper"></div>");
+
+	wrapper.append(Header());
+	wrapper.append(Todo());
+	root.apppend(wrapper);
+}
+
+const state = {
+	todos:[]
+};
+
+$( _ => {
+	const root = $(".root");
+	render(root);
+});
